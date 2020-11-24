4df18d0e0aabb2fe63655613ff8ad6163a7e3844
Shahan Ali Memon <samemon@andrew.cmu.edu>
2020-11-24 20:35:50 +0400

    added the docstring

---
+++ api/scrape.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+This api will be used to scrape data 
+from the F10000Research site. It 
+is a wrapper around the already 
+existent api.
+"""
+
+__author__ = "Shahan Ali Memon"
+__copyright__ = "Copyright 2021, NYUAD"
+__credits__ = ["Bedoor AlShebli"]
+__version__ = "1.0.0"
+__maintainer__ = "Shahan Ali Memon"
+__email__ = "shahan@nyu.edu"
+
